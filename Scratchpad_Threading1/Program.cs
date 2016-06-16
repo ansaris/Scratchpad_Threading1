cdfddce1833a236b8110b779c464cbc79819e6b5
ansaris <salman.ansari.usa@gmail.com>
2016-06-16 08:00:11 -0500

    Finish video 3 from Jamie King's Threading Youtube video playlist.

--- Scratchpad_Threading1/Program.cs
+++ Scratchpad_Threading1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Scratchpad_Threading1
 {
@@ -10,6 +11,24 @@ namespace Scratchpad_Threading1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            for (int i = 1; i <= Environment.ProcessorCount; i++)
+            {
+                Thread thread = new Thread(DifferentMethod);
+                thread.IsBackground = true;
+                thread.Start(i);
+            }
+            //Thread thread = new Thread(DifferentMethod);
+            //thread.Start();
+            //while (true)
+            //    Console.WriteLine("Hello from Main()");
+            Console.WriteLine("Leaving Main()");
+        }
+        
+        static void DifferentMethod(object threadID)
+        {
+            while(true)
+                Console.WriteLine($"Hello from DifferentMethod -- {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
